8a1dcfa203baaa6c034b835fdbec4a49c1b81792
simplebandaaaa <simplebandaa@outlook.com>
2026-08-01 13:39:02 +0530

    Add cleanup for X11/Xorg logs and lock files
    Clean up old X11/Xorg logs and lock files to prevent conflicts before generating RDP keys.

--- start.sh
+++ start.sh
@@ -11,7 +11,13 @@ mkdir -p /var/run/xrdp
 mkdir -p /var/run/xrdp/sockdir
 chown -R xrdp:xrdp /var/run/xrdp
 
-# जनरेट करें RDP कीज़ (अगर पहले से मौजूद नहीं हैं)
+# X11/Xorg के पुराने लॉग्स और लॉक फाइल्स साफ़ करें ताकि कोई कॉन्फ्लिक्ट न हो
+rm -f /tmp/.X*lock
+rm -rf /tmp/.X11-unix
+mkdir -p /tmp/.X11-unix
+chmod 1777 /tmp/.X11-unix
+
+# जनरेट करें RDP कीज़
 if [ ! -f /etc/xrdp/rsakeys.ini ]; then
     xrdp-keygen xrdp /etc/xrdp/rsakeys.ini
 fi
