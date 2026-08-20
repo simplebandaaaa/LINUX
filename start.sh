efba4a76afb795217e2e7af2d3f5a808af8d69cc
simplebandaaaa <simplebandaa@outlook.com>
2026-08-20 14:59:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-# D-Bus daemon system bus ko start aur setup karein
+# D-Bus folders aur Machine ID setup
 mkdir -p /var/run/dbus
+dbus-uuidgen --ensure=/etc/machine-id
+dbus-uuidgen --ensure=/var/lib/dbus/machine-id
 dbus-daemon --system --fork
 
-# XRDP services start karein
+# XRDP services start
 service xrdp start
 service xrdp-sesman start
 
-# Container ko exit hone se rokne ke liye logs ko tail karein
+# Logs tail karke container ko running rakhein
 tail -f /var/log/xrdp.log /var/log/xrdp-sesman.log
